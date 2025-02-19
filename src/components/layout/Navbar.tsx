d6ed9238ebb40a4b5a2086810d6406e50cecc304
Sander <82063944+Alexsander33set@users.noreply.github.com>
2025-02-18 23:22:30 -0300

    Redirect to `/` when click to logo on navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,15 +6,17 @@ import UserAvatar from "../Profile";
 export default async function Navbar() {
   return(
     <section id="navbar" className="flex justify-between items-center px-4 py-2">
-      <div className="logo flex items-center gap-4">
-        <Image src='/logo.webp' alt="Logo" height={28} width={28} />
-        <h1 className="font-medium text-xl">TaskTide</h1>
-      </div>
+      <Link href="/">
+        <div className="logo flex items-center gap-4">
+          <Image src='/logo.webp' alt="Logo" height={28} width={28} />
+          <h1 className="font-medium text-xl">TaskTide</h1>
+        </div>
+      </Link>
       <div className="flex items-center gap-2">
         <nav className="flex gap-4">
           <Link href="/">Home</Link>
           <Link href="/about">About</Link>
-          <Link href="/">Link3</Link>
+          <Link href="/user">User</Link>
           <Link href="/">Link4</Link>
         </nav>
         <UserAvatar />
