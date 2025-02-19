6b26574ad1c37acd7295d4e335e942bf960c8924
Sander <82063944+Alexsander33set@users.noreply.github.com>
2025-02-19 19:32:19 -0300

    Update Board component style

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,17 +1,30 @@
+"use client"
 // import { ReactNode } from "react";
 
 import Column from "./Column";
+import SettingsIcon from "./icons/Settings";
 
-export default function Board(
-  // { children }: { children: ReactNode }
-) {
+export default function Board() {
   const columns = ['Backlog', 'To Do', 'Doing', 'Done'];
 
   return (
-    <section className=" p-4 grid grid-cols-4 gap-4 bg-slate-200 shadow-md">
-      {columns.map((column) => (
-        <Column key={column} title={column} />
-      ))}
+    <section className="p-4 overflow-x-auto overflow-hidden">
+      <div id="board-header" className="flex justify-between items-center">
+      <div>
+        <h3 className="text-2xl">Board Name</h3>
+        <p>Decription</p>
+        </div>
+      <div className="flex items-center gap-4">
+      <button className="hover:bg-white py-1 px-2 rounded transition">+ Add Column</button>
+      <button><SettingsIcon className="text-gray-800" /></button>
+      </div>
+
+      </div>
+      <div className={`grid grid-flow-col mt-4 gap-4`}>
+        {columns.map((column) => (
+          <Column key={column} title={column} />
+        ))}
+      </div>
     </section>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,8 @@
-// import { ReactNode } from "react";
-
-export default function Card(
-  // { children }: { children: ReactNode }
-) {
+export default function Card({showCardDescription=false}) {
   return (
-    // <div className="card">{children}</div>
-    <>
-    Card
-    </>
+    <article className="p-4 rounded-lg shadow-md px-4 bg-white">
+    <div>Card Name</div>
+    {showCardDescription && <div>Card Description</div>}
+    </article>
   )
 }
