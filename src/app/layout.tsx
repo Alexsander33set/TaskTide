225f85d5de22d2c1884f4e4ad8618a4fd44c44ee
Sander <82063944+Alexsander33set@users.noreply.github.com>
2025-03-05 19:33:14 -0300

    Add `SessionProvider` to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,32 @@
 import { Inter } from "next/font/google";
-
-
+import { SessionProvider } from "next-auth/react";
 import type { Metadata } from "next";
+
 export const metadata: Metadata = {
-  title: 'TaskTide',
-  description: 'Manage your tasks efficiently with TaskTide',
+  title: "TaskTide",
+  description: "Manage your tasks efficiently with TaskTide",
   openGraph: {
-    title: 'TaskTide',
-    description: 'Manage your tasks efficiently with TaskTide',
-    images: ['logo.png'],
-  }
+    title: "TaskTide",
+    description: "Manage your tasks efficiently with TaskTide",
+  },
 };
 
 import "@/styles/globals.css";
 import Footer from "@/components/layout/Footer";
 import Navbar from "@/components/layout/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-      <html lang="en" suppressHydrationWarning>
-        <body className={`${inter.className} min-h-screen flex flex-col`}>
-          <Navbar/>
-          <main className="flex-1">
-            {children}
-          </main>
-          <Footer/>
-        </body>
-      </html>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} min-h-screen flex flex-col`}>
+        <SessionProvider>
+          <Navbar />
+          <main className="flex-1">{children}</main>
+          <Footer />
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
